648b28a9ba0ac1adea1b24fcaf14461d71664362
juanchigallego <hello@juanchigallego.com>
2024-12-11 16:01:32 -0300

    Initial Commit

---
+++ GymBuddy/GymBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBuddyApp.swift
+//  GymBuddy
+//
+//  Created by Juanchi Gallego on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GymBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymBuddyTests/GymBuddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  GymBuddyTests.swift
+//  GymBuddyTests
+//
+//  Created by Juanchi Gallego on 11/12/2024.
+//
+
+import Testing
+
+struct GymBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
